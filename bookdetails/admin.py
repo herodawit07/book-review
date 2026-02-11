2cf36febd2f669baaa6cec210ab6d183f7b83a67
Herodawit <herodawit0717@icloud.com>
2026-02-11 20:11:36 +0300

    Update admin.py

--- bookdetails/admin.py
+++ bookdetails/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-# bookdetails/admin.py
-from django.contrib import admin
 from .models import Book, Comment
 
 admin.site.register(Book)
